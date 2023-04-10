abdf4a391bfaea07d1acaa494233bf3f8520c2e8
LucasMiranda <lucaswespras@gmail.com>
2023-04-09 21:47:39 -0300

    entrada

---
+++ src/main/java/com/primeirotrampodev/exerciciocarros/ExercicioCarrosApplication.java
@@ -0,0 +1,13 @@
+package com.primeirotrampodev.exerciciocarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioCarrosApplication.class, args);
+	}
+
+}
